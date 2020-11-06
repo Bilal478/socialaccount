8b4720a84778d1cea37e8a25a0423291bf6fdbf1
mashcraft478 <bilalrasool.mashkraft@gmail.com>
2020-11-06 10:47:00 +0500

    .env updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-               {{$total}} 
+               {{$total}} Users 
                 </div>
 
                 <div class="links">
